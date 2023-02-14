69cf818c5691f995611bce013db5ace9e0564e90
Mehmet Akif Döne <87556588+akifdone@users.noreply.github.com>
2023-02-15 01:54:31 +0300

    Update ft_str_is_printable.c

--- c02/ex06/ft_str_is_printable.c
+++ c02/ex06/ft_str_is_printable.c
@@ -18,7 +18,7 @@ int	ft_str_is_printable(char *str)
 	count = 0;
 	while (str[count] != '\0')
 	{
-		if (str[count] > 32 && str[count] < 127)
+		if (str[count] >= 32 && str[count] <= 126)
 		{
 			++count;
 		}
