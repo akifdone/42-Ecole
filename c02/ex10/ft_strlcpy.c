7bd2e762c5594709020ba72bb29d1600497828b0
Mehmet Akif Döne <87556588+akifdone@users.noreply.github.com>
2023-02-15 01:53:44 +0300

    Update ft_strlcpy.c

--- c02/ex10/ft_strlcpy.c
+++ c02/ex10/ft_strlcpy.c
@@ -13,24 +13,17 @@
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
 	unsigned int	count;
-	unsigned int	i;
 
 	count = 0;
-	i = 0;
 	if (size != 0)
 	{
-		while (src[i] != '\0' && i < (size - 1))
+		while (src[count] != '\0' && count < (size - 1))
 		{
-			dest[i] = src[i];
-			i++;
+			dest[count] = src[count];
+			++count;
 		}
-		dest[i] = '\0';
-		
-		while (dest[count] != '\0')
-		{
-		    count++;
-		}
-    	   	count++;
+		dest[count] = '\0';
+    	++count;
 	}
 	return (count);
 }
