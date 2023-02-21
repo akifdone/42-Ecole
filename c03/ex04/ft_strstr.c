7310d0f274335b8297ae0e14fd6870a1a165d48c
Mehmet Akif Döne <87556588+akifdone@users.noreply.github.com>
2023-02-21 20:47:12 +0300

    Add files via upload

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: mdone <mdone@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 05:17:30 by mdone             #+#    #+#             */
-/*   Updated: 2023/02/12 05:17:31 by mdone            ###   ########.fr       */
+/*   Updated: 2023/02/17 09:52:27 by mdone            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ char	*ft_strstr(char *str, char *to_find)
 	int	count;
 
 	index = 0;
-	while (str[index] != '\0')
+	if (to_find[0] == '\0')
+		return (str);
+	while (str[index])
 	{
 		count = 0;
 		if (to_find[count] == str[index])
